6d20fe04f7a7e94f5dbd51dab8a3d3b955d75079
Leandro <leandro.soares.eneterio@gmail.com>
2025-02-28 10:56:37 -0300

    0.5

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { cn } from "@/lib/utils"
 import { Inter } from "next/font/google"
 import Header from "@/components/Header"
+import { ThemeProvider } from "@/components/ui/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,17 +20,19 @@ export default function DashboardLayout({
 }) {
     return (
         <html lang="pt-br" suppressHydrationWarning>
-            <body className={cn("min-h-screen bg-background font-sans antialiased relative w-full overflow-x-hidden", inter.className)}>
-                <Header />
-                <SidebarProvider open={true}>
-                    <AppSidebar />
-                    <main className="">
-                        <SidebarTrigger />
-                        {children}
-                    </main>
-                </SidebarProvider>
-                <Footer />
-            </body>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                <body className={cn("min-h-screen bg-background font-sans antialiased relative w-full overflow-x-hidden", inter.className)}>
+                    <Header />
+                    <SidebarProvider open={true}>
+                        <AppSidebar />
+                        <main className="">
+                            <SidebarTrigger />
+                            {children}
+                        </main>
+                    </SidebarProvider>
+                    <Footer />
+                </body>
+            </ThemeProvider>
         </html>
     )
 }
