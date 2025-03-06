d4158f7a91458d9a4f744c8eedac2135348be35c
Leandro <leandro.soares.eneterio@gmail.com>
2025-03-06 15:57:43 -0300

    0.5

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,10 @@ export async function requireAdmin() {
     const session = await getServerSession(authOptions);
 
     if (!session?.user?.id) return redirect("/signin");
-    if (session.user.role !== "admin") return redirect("/unauthorized");
+    if (!["admin", "owner"].includes(session.user.role)) {
+        return redirect("/unauthorized");
+    }
+
 
     return session; // Retorna a sessão caso seja admin
 }
