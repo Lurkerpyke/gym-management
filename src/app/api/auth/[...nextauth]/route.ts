ff136095996d07e1f6869c455b383eda7dd3d911
Leandro <leandro.soares.eneterio@gmail.com>
2025-03-29 16:43:39 -0300

    1.0

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,5 @@ import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
-// Only export the handlers, remove authOptions export
-// Only export the handlers, remove authOptions export
+
 export { handler as GET, handler as POST, authOptions };
